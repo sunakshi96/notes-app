7a74782b0930ed1caf1c15e78fa1d571241393d1
sunakshi96 <sunakshitejwani@gmail.com>
2020-02-23 15:23:39 +0530

    adding the note and checking the duplicacy of notes

--- notes.js
+++ notes.js
@@ -1,12 +1,43 @@
-const getNotes = function() {
-    return("My mission is one world family!");
-}
+const fs = require("fs");
 
-const addNote = function (title,body){
-    
-}
+const getNotes = function() {
+  return "Your notes...";
+};
+
+const addNote = function(title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function(note) {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    saveNotes(notes);
+    console.log("New Note added!");
+  } else {
+    console.log("Note title taken already!");
+  }
+};
+
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
 
 module.exports = {
-    getNotes: getNotes,
-    addNote: addNote
+  getNotes: getNotes,
+  addNote: addNote
 };
